13abbe1f7708345068ce7ef523e0dddc44434a67
sairevanth00 <you@example.com>
2021-08-11 16:16:17 +0000

    debugging

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-named-as-default-member
+import BlogList from '../BlogList'
+
+import UserInfo from '../UserInfo'
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
